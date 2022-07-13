077cbe133c2c41d7cc4df709f18a7f4ea30862b8
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-07-13 18:45:58 -0300

    feat: change appbar style

--- app/src/main/java/ar/edu/unlam/tiendabicicletas/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/tiendabicicletas/MainActivity.kt
@@ -41,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
             R.id.icon_login -> {
                 val navController = findNavController(R.id.nav_host_fragment_content_main)
                 navController.navigate(R.id.loginFragment)
